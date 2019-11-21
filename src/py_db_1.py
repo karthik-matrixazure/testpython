6d014905c388e9ada2ba8d598338bbd1945f3c5f
karthik-matrixazure <57978767+karthik-matrixazure@users.noreply.github.com>
2019-11-21 12:50:44 +0530

    Update py_db_1.py

--- src/py_db_1.py
+++ src/py_db_1.py
@@ -1,23 +1,15 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Nov 20 15:06:32 2019
-
 @author: Hp
 """
-
 import pyodbc
-
 sql_conn = pyodbc.connect(DRIVER='{ODBC Driver 17 for SQL Server}',
-                            SERVER='tcp:pythonazuretest.database.windows.net,1433',
-                            DATABASE='PythonAzureTest',
-#                            Trusted_Connection='yes',
-                            uid='PythonAzureTest',
-                            pwd='Python@123',
-                            Encrypt='yes',
-                            TrustServerCertificate='no')
+           SERVER='tcp:pythonazuretest.database.windows.net,1433',
+           DATABASE='PythonAzureTest',  uid='PythonAzureTest', pwd='Python@123', Encrypt='yes',
+           TrustServerCertificate='no')
 cursor = sql_conn.cursor()
 cursor.execute('SELECT * FROM PythonAzureTest.dbo.input_test')
-
 row = cursor.fetchone()
 print(row)
 cursor.execute("INSERT INTO output_test(output_data) SELECT input_data FROM PythonAzureTest.dbo.input_test;")
@@ -29,6 +21,3 @@ sql_conn.commit()
 print("Data Successfully Inserted")   
 sql_conn.close()  
 
-
-
-#dns = 'Driver={ODBC Driver 13 for SQL Server};Server=tcp:pythonazuretest.database.windows.net,1433;Database=PythonAzureTest;Uid=PythonAzureTest;Pwd={your_password_here};Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30'
